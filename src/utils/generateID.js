91efb49ba315b4c3867cc250f1b6d5972b470a98
Мухутдинов Искандер <mi@smarthead.ru>
2020-06-26 18:11:14 +0300

    Добавляет описание ютилсу

--- src/utils/generateID.js
+++ src/utils/generateID.js
@@ -1,3 +1,11 @@
+/**
+ * Функция генерирует уникальные ID
+ *
+ * @name generateID
+ *
+ * @return {string} получаемый ID в виде строки
+ */
+
 export default function generateID() {
     return '_' + Math.random().toString(36).substr(2, 9);
 };
